f506a2a4bb4c302cafa8e98e63262756fc0af25f
pkoly <pkolyvanis+github@gmail.com>
2020-10-01 23:41:09 +0100

    parser implementation and tests

---
+++ tests/fixtures.js
@@ -0,0 +1,8 @@
+exports.htmlSnippet = [
+    "<div>",
+    '<a href="apple.html" title="Apple" />',
+    '<a href="orange.html" title="Orange" />',
+    '<img src="banana.png" alt="Banana" />',
+    '<a href="pear.html" title="Pear" />',
+    "</div>",
+].join("");
